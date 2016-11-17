943c47dec10f05ec351edad52c3efdab64f1adb3
adsouza31 <ajaydsouza@gatech.edu>
2016-11-17 13:06:27 -0800

    -a

--- source/US24TourPlot.m
+++ source/US24TourPlot.m
@@ -1,10 +1,24 @@
 clear all;
 
+%outputfile = 'US24.output';
 outputfile = 'US24.output';
 f = fopen(outputfile,'r');
 x = fscanf(f, '%d\t %d', [2, inf]);
 fclose(f);
 x = x';
+
+% read the list of cities
+cities = cell(1e6, 2) ;
+citiesfile = 'cities.input';
+f = fopen(citiesfile,'r');
+rCnt = 0 ; 
+while ~feof(f)
+    rCnt = rCnt + 1 ;
+    cities{rCnt,1} = fscanf(f, '%d', 1) ;
+    cities{rCnt,2} = fscanf(f, '%s', 1) ;
+end
+fclose(f);
+cities = cities(1:rCnt-1,:) ;
 
 coordfile = 'US48.input';
 f = fopen(coordfile,'r');
@@ -21,6 +35,10 @@ for k = 1 : 24
 end
 figure(1);
 plot(coord(tour(:,1),2),coord(tour(:,1),3),'rO');
+text(coord(tour(:,1),2),coord(tour(:,1),3),num2str(coord(tour(:,1),1)),'HorizontalAlignment','center','VerticalAlignment','bottom')
+text(coord(tour(:,1),2),coord(tour(:,1),3), cities(tour(:,1),2),'HorizontalAlignment','center','VerticalAlignment','top')
+
+
 hold on;
 plot([coord(tour(:,1),2);coord(1,2)],[coord(tour(:,1),3);coord(1,3)]);
 plot(coord(1,2),coord(1,3),'r*'); % the star marks Atlanta
